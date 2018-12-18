5576b8626f1cf39af1bda5b7b03fb74c5510cc5d
Miguel <miguel.qma@gmail.com>
2018-12-18 17:11:24 -0300

    crud, y hateoas dependency

---
+++ src/main/java/com/practica/demo/payroll/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.practica.demo.payroll;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+
+
+
+}
